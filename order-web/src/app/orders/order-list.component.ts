013ec541515a46960806be692bbb8facb8aac88c
tranchau <tranchau>
2023-03-12 18:05:43 +0700

    chinh sua them chuc nang export excel

--- order-web/src/app/orders/order-list.component.ts
+++ order-web/src/app/orders/order-list.component.ts
@@ -22,6 +22,7 @@ import { AgencyService } from '../services/agency.service';
 import { ToastrService } from 'ngx-toastr';
 import { TranslateService } from '@ngx-translate/core';
 import * as XLSX from 'xlsx';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-order-list',
@@ -80,6 +81,7 @@ export class OrderListComponent implements AfterViewInit, OnInit {
     private agencyService: AgencyService,
     private toastr: ToastrService,
     public translate: TranslateService,
+    protected sanitizer: DomSanitizer,
   ) { }
 
   ngOnInit(): void {
@@ -87,11 +89,11 @@ export class OrderListComponent implements AfterViewInit, OnInit {
     this.agencyList = this.helper.getAgencyList();
     // const orderList = this.helper.getOrderList();
     // if (orderList.length === 0) {
-      this.orderService.getOrderList().subscribe((response: any) => {
-        console.log(response)
-        this.helper.setOrderList(response);
-        this.dataSource.data = response.length > 0 ? response.reverse() : [];
-      });
+    this.orderService.getOrderList().subscribe((response: any) => {
+      console.log(response)
+      this.helper.setOrderList(response);
+      this.dataSource.data = response.length > 0 ? response.reverse() : [];
+    });
     // } else {
     //   this.dataSource.data = orderList.length > 0 ? orderList.reverse() : [];
     // }
@@ -166,17 +168,61 @@ export class OrderListComponent implements AfterViewInit, OnInit {
   }
 
   onPrint(row: any) {
-    // xuat don dat hang
-    alert('In chi tiết đơn hàng dạng pdf')
+    //var blob = new Blob([row.blob()], { type: 'application/pdf' });
+    // const blobUrl = URL.createObjectURL(blob);
+    //   const iframe = document.createElement('iframe');
+    //   iframe.style.display = 'none';
+    //   iframe.src = blobUrl;
+    //   document.body.appendChild(iframe);
+    //   iframe.contentWindow ? iframe.contentWindow.print() : null;
+
+
+    const pdf = new Blob([row], { type: 'application/pdf' });
+    const blobUrl = URL.createObjectURL(pdf);
+    const iframe = document.createElement('iframe');
+
+    iframe.style.display = 'none';
+    //iframe.src = this.sanitizer.sanitize(SecurityContext.RESOURCE_URL, this.sanitizer.bypassSecurityTrustResourceUrl(blobUrl));
+    document.body.appendChild(iframe);
+    iframe.contentWindow ? iframe.contentWindow.print() : null;
   }
 
   exportToExcel() {
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.TABLE.nativeElement);
-    ws['!cols'] = [];
-    ws['!cols'][11] = { hidden: true };
+    const data = this.dataSource.data.map(c => ({ 
+      'Mã số đơn hàng': c.id, 
+      'Ngày tạo đơn': c.createdDate,
+      'Nhà phân phối': this.agencyList.find(x => x.id === c.agencyId)?.fullName,
+      'Hợp đồng': c.contract,
+      'Ngày nhận dự kiến': c.receivedDate,
+      'Nơi nhận': this.deliveries.find(x => x.id === c.deliveryId)?.label,
+      'Nơi giao': this.cities.find(x => x.id === c.pickupId)?.label,
+      'Sản phẩm': this.getProductName(c.products),
+      'Số lượng': this.getProductQuantity(c.products),
+      'Tổng số lượng': c.productTotal,
+      'Số phương tiện': c.licensePlates,
+      'Tên tài xế': c.driver,
+    }));
+    const ws = XLSX.utils.json_to_sheet(data);
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    wb.Sheets['Sheet1']['H2'].s = {alignment:{ wrapText: true }};
     XLSX.writeFile(wb, this.fileName);
+  }
+
+  getProductName(products: any[]): string {
+    let str = '';
+    products.forEach(el => {
+      str += el.name + ',\n';
+    });
+    return str;
+  }
+
+  getProductQuantity(products: any[]): string {
+    let str = '';
+    products.forEach(el => {
+      str += el.quantity + ',\n';
+    });
+    return str;
   }
 
   onSearch() {
